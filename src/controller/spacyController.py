675dfdedc6efdf3622e3212a8c8d00fd6fa22b41
kiri23 <christian@opticpower.com>
2022-06-27 10:48:37 -0400

    add fake and editable bank statement

--- src/controller/spacyController.py
+++ src/controller/spacyController.py
@@ -1,7 +1,7 @@
 import spacy
 from spacy import displacy
 
-# download free templates on https://templatelab.com/bank-statement/#google_vignette or check for bank fake template
+# download free templates on https://templatelab.com/bank-statement/#google_vignette or check for bank fake template like this https://templatearchive.com/bank-statement/#google_vignette
 nlp = spacy.load("en_core_web_sm")
 doc = nlp("")
 html = displacy.render(doc, style="ent", page=True)
